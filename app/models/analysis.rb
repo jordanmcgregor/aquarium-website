b6901bac701e85d64db2b799d93cc83d3212c58e
Jordan McGregor <jordanmmcgregor@gmail.com>
2020-10-13 21:59:55 -0600

    made it so the slug can be changed

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -3,4 +3,8 @@ class Analysis < ApplicationRecord
   has_many :factors
   extend FriendlyId
   friendly_id :analysis_article_title, use: :slugged
+
+  def should_generate_new_friendly_id? #will change the slug if the name changed
+    analysis_article_title_changed?
+  end
 end
